68d2af0015524b801e400a71fb7e9737dc47d881
wikivitas <71380540+wikivitas@users.noreply.github.com>
2022-09-15 12:57:10 +0300

    Create nodarbiba_1_1_ievads.m

---
+++ nodarbiba_1_1_ievads.m
@@ -0,0 +1,6 @@
+%% 2.piemērs
+clc, clearvars, format compact
+a_isa = 787/9+453/13 % Ctrl_Enter
+format long, a_gara = a_isa % Ctrl+Enter
+a_gar25 = vpa(a_isa, 25) % Ctrl+Enter
+format % atgriezties(pēc noklus.)
